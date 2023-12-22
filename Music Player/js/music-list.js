167fe1a60c42c3193330fb1316e7a4cd35059bbc
Atishay <atishaysoni1801@gmail.com>
2023-12-22 22:01:20 +0530

    Music_Player, Text_to_Speech, Weather_app added and committed

---
+++ Music Player/js/music-list.js
@@ -0,0 +1,33 @@
+
+let allMusic = [
+    {
+      name: "On & On",
+      artist: "Cartoon, Daniel Levi",
+      img: "On_and_On_img",
+      src: "On-and-On"
+    },
+    {
+      name: "The Drum",
+      artist: "Alan Walker",
+      img: "The_Drum_img",
+      src: "The-Drum"
+    },
+    {
+        name: "Hymn for the Weekend",
+        artist: "Coldplay",
+        img: "Hymn_For_The_Weekend_img",
+        src: "Hymn-For-The-Weekend"
+    },
+    {
+      name: "Something Just Like This",
+      artist: "The Chainsmokers, Coldplay",
+      img: "Something_Just_Like_This_img",
+      src: "Something-Just-Like-This"
+    },
+    {
+      name: "Shape of You",
+      artist: "Ed Sheeran",
+      img: "Shape_Of_You_img",
+      src: "Shape-Of-You"
+    },
+  ];
